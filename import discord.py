6432c3acc467e6ef1fc66db2c9b8dcb285f3f1c8
miluty <145078069+miluty@users.noreply.github.com>
2025-04-22 05:32:47 +0200

    Update import discord.py

--- import discord.py
+++ import discord.py
@@ -2,25 +2,29 @@ import discord
 import os
 
 
-TOKEN = os.getenv("MTI3ODg0NjYzNjYxNzEwOTYzNw.Gc8wUQ.P0mYNwZXQUwiX4MO4gQCOeFinUbVQ1VMal2hjg")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 
 intents = discord.Intents.default()
-intents.message_content = True
-
+intents.message_content = True  
 
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
     print(f'✅ Bot conectado como {client.user}')
 
+
 @client.event
 async def on_message(message):
+   
     if message.author == client.user:
         return
 
+ 
     if message.content.lower() == "!ping":
         await message.channel.send("🏓 Pong!")
 
+# Iniciar el bot
 client.run(TOKEN)
